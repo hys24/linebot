726bbaf4fe1fe17b1b778c80172612481585eaad
hayashitakahiro <Me@hayashitakahiro-no-iMac.local>
2017-10-02 23:20:34 +0900

    a

--- index.php
+++ index.php
@@ -53,7 +53,7 @@ foreach ($events as $event) {
     try{
       $stmt = $pdo->query($sql);
       while($result = $stmt->fetch(PDO::FETCH_ASSOC)){
-        if(in_array($messageText, $result))array_push($array, $result);
+        if(in_array($messageText, $result))error_log(print_r($result,true));//array_push($array, $result);
       }
     }catch (PDOException $e){
       error_log('Error:'.$e->getMessage());
